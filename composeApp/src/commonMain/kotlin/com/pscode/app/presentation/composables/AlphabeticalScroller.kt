937b1a23f06b39668597871a5cd4561ea032138f
noner <pawel495@interia.pl>
2023-12-06 14:08:52 +0100

    Removing ripple effect from AlphabeticalScroller

--- composeApp/src/commonMain/kotlin/com/pscode/app/presentation/composables/AlphabeticalScroller.kt
+++ composeApp/src/commonMain/kotlin/com/pscode/app/presentation/composables/AlphabeticalScroller.kt
@@ -1,12 +1,14 @@
 package com.pscode.app.presentation.composables
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -16,19 +18,21 @@ fun AlphabeticalScroller(
     onLetterClick: (Char) -> Unit, modifier: Modifier = Modifier
 ) {
     val alphabet = ('A'..'Z')
+    val interactionSource = remember { MutableInteractionSource() }
+
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         alphabet.forEach { letter ->
-            Text(
-                text = letter.uppercase(),
+            Text(text = letter.uppercase(),
                 color = MaterialTheme.colorScheme.primary,
-                modifier = Modifier
-                    .clickable { onLetterClick(letter) }
-                    .padding(vertical = 1.dp)
-            )
+                modifier = Modifier.clickable(
+                        interactionSource = interactionSource, indication = null
+                    ) {
+                        onLetterClick(letter)
+                    }.padding(vertical = 1.dp))
         }
     }
 }
