cab1fe5cd20e71a565a18c1961fddc555718829f
noner <pawel495@interia.pl>
2023-12-06 18:37:21 +0100

    Adding search and scroll detection

--- composeApp/src/commonMain/kotlin/com/pscode/app/presentation/screens/countries/overview/OverviewScreen.kt
+++ composeApp/src/commonMain/kotlin/com/pscode/app/presentation/screens/countries/overview/OverviewScreen.kt
@@ -1,28 +1,42 @@
 package com.pscode.app.presentation.screens.countries.overview
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import com.pscode.app.SharedRes
 import com.pscode.app.domain.model.Country
 import com.pscode.app.domain.repository.CountryRepository
 import com.pscode.app.presentation.composables.AlphabeticalScroller
@@ -43,14 +57,19 @@ class OverviewScreen(val onShowSnackBar: (String) -> Unit) : Screen {
         val viewModel = getViewModel(
             Unit,
             viewModelFactory { OverviewViewModel(countryRepository = countryRepository) })
-        val state by viewModel.state.collectAsState()
-        val groupedCountries = state.countries.groupBy { it.name.first() }
+        val isLoading by viewModel.isLoading.collectAsState()
+        val isSearching by viewModel.isSearching.collectAsState()
+        val countries by viewModel.countries.collectAsState()
+        val searchText by viewModel.searchText.collectAsState()
+
+        val groupedCountries = countries.groupBy { it.name.first() }
         val errorsChannel = viewModel.errorEventsChannelFlow
 
         val navigator = LocalNavigator.currentOrThrow
 
         val scope = rememberCoroutineScope()
         val lazyListState = rememberLazyListState()
+        val showSearch = lazyListState.isScrollingUp()
 
         LaunchedEffect(errorsChannel) {
             errorsChannel.collect { event ->
@@ -62,47 +81,70 @@ class OverviewScreen(val onShowSnackBar: (String) -> Unit) : Screen {
             }
         }
 
-        Row(
+        Column(
             modifier = Modifier.fillMaxSize().padding(horizontal = 8.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-            LazyColumn(
-                state = lazyListState,
-                modifier = Modifier.fillMaxHeight().fillMaxWidth(0.9f),
-                verticalArrangement = Arrangement.spacedBy(4.dp)
-            ) {
-                groupedCountries.forEach { (letter, countries) ->
-                    stickyHeader {
-                        LetterHeader(
-                            letter,
-                            modifier = Modifier.fillMaxWidth()
-                                .background(color = MaterialTheme.colorScheme.surface)
-                                .padding(vertical = 4.dp, horizontal = 8.dp)
-                        )
-                    }
 
-                    items(items = countries) { country ->
-                        CountryListItem(
-                            countryName = country.name,
-                            flagUrl = country.flagUrl,
-                            onCountryClick = { countryName ->
-                                navigator.push(item = DetailScreen(countryName = countryName))
-                            },
-                            modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
-                        )
-                    }
-                }
+            AnimatedVisibility(visible = showSearch) {
+                OutlinedTextField(
+                    value = searchText,
+                    onValueChange = { viewModel.onSearchTextChange(it) },
+                    leadingIcon = {
+                        Icon(imageVector = Icons.Default.Search, contentDescription = null)
+                    },
+                    placeholder = {
+                        Text(text = SharedRes.string.search)
+                    },
+                    shape = RoundedCornerShape(percent = 30),
+                    modifier = Modifier.fillMaxWidth()
+                )
             }
 
-            AlphabeticalScroller(
-                onLetterClick = { clickedLetter ->
-                    val index = getScrollIndex(groupedCountries, clickedLetter)
-                    scope.launch {
-                        lazyListState.animateScrollToItem(index)
+            Row(
+                modifier = Modifier.fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                LazyColumn(
+                    state = lazyListState,
+                    modifier = Modifier.fillMaxHeight().fillMaxWidth(0.9f),
+                    verticalArrangement = Arrangement.spacedBy(4.dp)
+                ) {
+
+                    groupedCountries.forEach { (letter, countries) ->
+                        stickyHeader {
+                            LetterHeader(
+                                letter,
+                                modifier = Modifier.fillMaxWidth()
+                                    .background(color = MaterialTheme.colorScheme.surface)
+                                    .padding(vertical = 4.dp, horizontal = 8.dp)
+                            )
+                        }
+
+                        items(items = countries) { country ->
+                            CountryListItem(
+                                countryName = country.name,
+                                flagUrl = country.flagUrl,
+                                onCountryClick = { countryName ->
+                                    navigator.push(item = DetailScreen(countryName = countryName))
+                                },
+                                modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
+                            )
+                        }
                     }
-                }, modifier = Modifier.fillMaxHeight().fillMaxWidth()
-            )
+                }
+
+                AlphabeticalScroller(
+                    onLetterClick = { clickedLetter ->
+                        val index = getScrollIndex(groupedCountries, clickedLetter)
+                        scope.launch {
+                            lazyListState.animateScrollToItem(index)
+                        }
+                    }, modifier = Modifier.fillMaxHeight().fillMaxWidth()
+                )
+            }
         }
     }
 
@@ -115,6 +157,24 @@ class OverviewScreen(val onShowSnackBar: (String) -> Unit) : Screen {
             index += 1 + value.size
         }
         return index
+    }
+
+    @Composable
+    private fun LazyListState.isScrollingUp(): Boolean {
+        var previousIndex by remember(this) { mutableStateOf(firstVisibleItemIndex) }
+        var previousScrollOffset by remember(this) { mutableStateOf(firstVisibleItemScrollOffset) }
+        return remember(this) {
+            derivedStateOf {
+                if (previousIndex != firstVisibleItemIndex) {
+                    previousIndex > firstVisibleItemIndex
+                } else {
+                    previousScrollOffset >= firstVisibleItemScrollOffset
+                }.also {
+                    previousIndex = firstVisibleItemIndex
+                    previousScrollOffset = firstVisibleItemScrollOffset
+                }
+            }
+        }.value
     }
 
 }
--- composeApp/src/commonMain/kotlin/com/pscode/app/presentation/screens/countries/overview/OverviewViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/pscode/app/presentation/screens/countries/overview/OverviewViewModel.kt
@@ -1,50 +1,110 @@
 package com.pscode.app.presentation.screens.countries.overview
 
+import com.pscode.app.domain.model.Country
 import com.pscode.app.domain.repository.CountryRepository
 import com.pscode.app.utils.Response
 import dev.icerock.moko.mvvm.viewmodel.ViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class OverviewViewModel(private val countryRepository: CountryRepository) : ViewModel() {
 
-    private val _state = MutableStateFlow(OverviewUiState())
-    val state = _state.asStateFlow()
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading = _isLoading.asStateFlow()
+
+    private val _searchText = MutableStateFlow("")
+    val searchText = _searchText.asStateFlow()
+
+    private val _isSearching = MutableStateFlow(false)
+    val isSearching = _isSearching.asStateFlow()
+
+    private var _countries = MutableStateFlow(emptyList<Country>())
+    val countries =
+        searchText.onEach { _isSearching.update { true } }.combine(_countries) { text, countries ->
+            if (text.isBlank()) {
+                countries
+            } else {
+                countries.filter {
+                    it.name.startsWith(prefix = text, ignoreCase = true)
+                }
+            }
+        }.onEach { _isSearching.update { false } }.stateIn(
+            viewModelScope, SharingStarted.WhileSubscribed(5000), _countries.value
+        )
 
     private val errorChannel = Channel<ErrorEvent>()
     val errorEventsChannelFlow = errorChannel.receiveAsFlow()
+
 
     init {
         getAllCountries()
     }
 
+    fun onSearchTextChange(text: String) {
+        _searchText.update { text }
+    }
 
     private fun getAllCountries() {
         viewModelScope.launch {
             when (val result = countryRepository.getAllCountries()) {
                 is Response.Success -> {
-                    _state.update {
-                        it.copy(
-                            isLoading = false, countries = result.data
-                        )
+                    _isLoading.update {
+                        false
+                    }
+                    _countries.update {
+                        result.data
                     }
                 }
 
                 is Response.Error -> {
                     errorChannel.send(ErrorEvent.ShowSnackbarMessage(message = result.message))
 
-                    _state.update {
-                        it.copy(
-                            isLoading = false
-                        )
-                    }
+                    _isLoading.update { false }
                 }
             }
         }
     }
-
 }
+
+
+//    private fun getAllCountries(searchText: String? = null) {
+//        viewModelScope.launch {
+//            when (val result = countryRepository.getAllCountries()) {
+//                is Response.Success -> {
+//                    if (!searchText.isNullOrEmpty()) {
+//                        _state.update {
+//                            it.copy(isLoading = false, countries = result.data.filter { country ->
+//                                country.name.contains(
+//                                    other = searchText, ignoreCase = true
+//                                )
+//                            })
+//                        }
+//                    } else {
+//                        _state.update {
+//                            it.copy(
+//                                isLoading = false, countries = result.data
+//                            )
+//                        }
+//                    }
+//                }
+//
+//                is Response.Error -> {
+//                    errorChannel.send(ErrorEvent.ShowSnackbarMessage(message = result.message))
+//
+//                    _state.update {
+//                        it.copy(
+//                            isLoading = false
+//                        )
+//                    }
+//                }
+//            }
+//        }
+//    }
