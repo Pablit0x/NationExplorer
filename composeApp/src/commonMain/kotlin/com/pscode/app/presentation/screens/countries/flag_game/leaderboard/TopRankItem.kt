dc6b15578968103be6c7cf0c6d2a0a8fe20af916
noner <pawel495@interia.pl>
2023-12-15 13:24:11 +0100

    Final touches to leaderboard

--- composeApp/src/commonMain/kotlin/com/pscode/app/presentation/screens/countries/flag_game/leaderboard/TopRankItem.kt
+++ composeApp/src/commonMain/kotlin/com/pscode/app/presentation/screens/countries/flag_game/leaderboard/TopRankItem.kt
@@ -1,7 +1,6 @@
 package com.pscode.app.presentation.screens.countries.flag_game.leaderboard
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,9 +8,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.WorkspacePremium
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
