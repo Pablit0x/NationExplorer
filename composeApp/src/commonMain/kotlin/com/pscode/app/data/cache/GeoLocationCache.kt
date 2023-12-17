461652fbd260113d9d1af892113131d3cd9fcc9c
noner <pawel495@interia.pl>
2023-12-17 21:18:50 +0100

    Adding support for 120hz on iPhone

--- composeApp/src/commonMain/kotlin/com/pscode/app/data/cache/GeoLocationCache.kt
+++
@@ -1,9 +0,0 @@
-package com.pscode.app.data.cache
-
-import com.pscode.app.domain.model.LocationOverview
-import io.github.xxfast.kstore.KStore
-import io.github.xxfast.kstore.file.storeOf
-
-class GeoLocationCache {
-    val cache: KStore<List<LocationOverview>> = storeOf(file = getGeoLocationCachePath().path)
-}
--- composeApp/src/commonMain/kotlin/com/pscode/app/data/cache/GeoLocationCachePath.kt
+++
@@ -1,9 +0,0 @@
-package com.pscode.app.data.cache
-
-import okio.Path
-
-interface GeoLocationCachePath {
-    val path: Path
-}
-
-expect fun getGeoLocationCachePath(): GeoLocationCachePath
