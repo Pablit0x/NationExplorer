5a71ebaf768988844aa0006a48ca4a9abce4d95e
noner <pawel495@interia.pl>
2024-01-10 21:50:31 +0100

    Polishing FilterView

--- composeApp/src/commonMain/kotlin/com/pscode/app/presentation/screens/countries/overview/FilterBottomSheet.kt
+++ composeApp/src/commonMain/kotlin/com/pscode/app/presentation/screens/countries/overview/FilterBottomSheet.kt
@@ -57,128 +57,127 @@ fun FilterBottomSheet(
         sheetState = sheetState,
         onDismissRequest = { onUpdateFilterWidgetState(FilterWidgetState.CLOSED) }) {
 
-        AutoResizedText(
-            text = SharedRes.string.filter_results,
-            style = MaterialTheme.typography.headlineMedium,
-            modifier = Modifier.fillMaxWidth()
-        )
+        Column(modifier = Modifier.padding(horizontal = 16.dp)){
 
-        Spacer(modifier = Modifier.height(16.dp))
+            AutoResizedText(
+                text = SharedRes.string.filter_results,
+                style = MaterialTheme.typography.headlineMedium,
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
 
 
-        Text(
-            text = SharedRes.string.population,
-            style = MaterialTheme.typography.labelLarge,
-            color = MaterialTheme.colorScheme.outline,
-            modifier = Modifier.padding(horizontal = 16.dp)
-        )
+            Text(
+                text = SharedRes.string.population,
+                style = MaterialTheme.typography.labelLarge,
+                color = MaterialTheme.colorScheme.outline
+            )
 
-        Row(
-            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            populationFilterItems.forEach { populationItem ->
-                FilterChip(
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                populationFilterItems.forEach { populationItem ->
+                    FilterChip(
 
-                    selected = populationItem.isSelected,
-                    label = {
-                        Text(
-                            text = "< " + convertPopulation(populationItem.label.toLong()),
-                            style = MaterialTheme.typography.labelMedium
-                        )
-                    },
-                    onClick = { onUpdatePopulationFilterItem(populationItem.label) },
-                    modifier = Modifier.padding(4.dp).weight(1f)
-                )
+                        selected = populationItem.isSelected,
+                        label = {
+                            Text(
+                                text = "< " + convertPopulation(populationItem.label.toLong()),
+                                style = MaterialTheme.typography.labelMedium
+                            )
+                        },
+                        onClick = { onUpdatePopulationFilterItem(populationItem.label) },
+                        modifier = Modifier.padding(4.dp).weight(1f)
+                    )
+                }
             }
-        }
 
-        Spacer(modifier = Modifier.height(8.dp))
+            Spacer(modifier = Modifier.height(8.dp))
 
-        Text(
-            text = SharedRes.string.continents,
-            style = MaterialTheme.typography.labelLarge,
-            color = MaterialTheme.colorScheme.outline,
-            modifier = Modifier.padding(horizontal = 16.dp)
-        )
 
-        LazyVerticalStaggeredGrid(
-            columns = StaggeredGridCells.Fixed(count = 3),
-            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 4.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            verticalItemSpacing = 4.dp,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            items(continentsFilterItems) { continent ->
-                ElevatedFilterChip(selected = continent.isSelected,
-                    onClick = { onUpdateContinentFilterItem(continent.label) },
-                    label = {
-                        Row(
-                            modifier = Modifier.fillMaxWidth().padding(4.dp),
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.spacedBy(4.dp)
-                        ) {
-                            AnimatedVisibility(continent.isSelected) {
-                                Icon(
-                                    imageVector = Icons.Default.Check,
-                                    contentDescription = "Filter Applied",
-                                    modifier = Modifier.size(16.dp)
+            Text(
+                text = SharedRes.string.continents,
+                style = MaterialTheme.typography.labelLarge,
+                color = MaterialTheme.colorScheme.outline
+            )
+
+            LazyVerticalStaggeredGrid(
+                columns = StaggeredGridCells.Fixed(count = 3),
+                contentPadding = PaddingValues(vertical = 4.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalItemSpacing = 4.dp,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                items(continentsFilterItems) { continent ->
+                    ElevatedFilterChip(selected = continent.isSelected,
+                        onClick = { onUpdateContinentFilterItem(continent.label) },
+                        label = {
+                            Row(
+                                modifier = Modifier.fillMaxWidth().padding(4.dp),
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.spacedBy(4.dp)
+                            ) {
+                                AnimatedVisibility(continent.isSelected) {
+                                    Icon(
+                                        imageVector = Icons.Default.Check,
+                                        contentDescription = "Filter Applied",
+                                        modifier = Modifier.size(16.dp)
+                                    )
+                                }
+                                Text(
+                                    text = continent.label, style = MaterialTheme.typography.labelSmall
                                 )
                             }
-                            Text(
-                                text = continent.label, style = MaterialTheme.typography.labelSmall
-                            )
-                        }
-                    })
+                        })
+                }
             }
-        }
 
-        Spacer(modifier = Modifier.height(4.dp))
+            Spacer(modifier = Modifier.height(4.dp))
 
-        Divider(modifier = Modifier.padding(horizontal = 16.dp))
+            Divider()
 
-        Spacer(modifier = Modifier.height(8.dp))
+            Spacer(modifier = Modifier.height(8.dp))
 
-
-        Row(
-            modifier = Modifier.fillMaxWidth().height(IntrinsicSize.Min)
-                .padding(horizontal = 16.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(
-                text = SharedRes.string.favourites_only,
-                color = MaterialTheme.colorScheme.onSurface,
-                style = MaterialTheme.typography.labelLarge
-            )
-            Switch(checked = showFavouritesOnly, onCheckedChange = { onToggleFavouriteOnly() })
-        }
-
-
-        Column(
-            modifier = Modifier.fillMaxWidth().height(90.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.End
-        ) {
-            AnimatedVisibility(
-                isFiltering or showFavouritesOnly, enter = fadeIn(), exit = fadeOut()
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                ElevatedButton(
-                    onClick = onResetAllFilters, modifier = Modifier.padding(
-                        top = 32.dp, bottom = 16.dp, start = 32.dp, end = 32.dp
-                    ).fillMaxWidth()
+                Text(
+                    text = SharedRes.string.favourites_only,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    style = MaterialTheme.typography.labelLarge
+                )
+                Switch(checked = showFavouritesOnly, onCheckedChange = { onToggleFavouriteOnly() })
+            }
+
+            Column(
+                modifier = Modifier.fillMaxWidth().height(90.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.End
+            ) {
+                AnimatedVisibility(
+                    isFiltering or showFavouritesOnly, enter = fadeIn(), exit = fadeOut()
                 ) {
-                    Icon(
-                        imageVector = Icons.Default.Clear,
-                        modifier = Modifier.size(24.dp),
-                        tint = MaterialTheme.colorScheme.error,
-                        contentDescription = "Clear all filters"
-                    )
-                    Spacer(modifier = Modifier.width(4.dp))
-                    Text(
-                        text = SharedRes.string.reset_filters, color = MaterialTheme.colorScheme.error
-                    )
+                    ElevatedButton(
+                        onClick = onResetAllFilters, modifier = Modifier.padding(
+                            top = 32.dp, bottom = 16.dp, start = 16.dp, end = 16.dp
+                        ).fillMaxWidth()
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Clear,
+                            modifier = Modifier.size(24.dp),
+                            tint = MaterialTheme.colorScheme.error,
+                            contentDescription = "Clear all filters"
+                        )
+                        Spacer(modifier = Modifier.width(4.dp))
+                        Text(
+                            text = SharedRes.string.reset_filters, color = MaterialTheme.colorScheme.error
+                        )
+                    }
                 }
             }
         }
